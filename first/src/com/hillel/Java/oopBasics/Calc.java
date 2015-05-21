54df1eb715c7e81dbb0bd27450150b99b339199e
abozhko <tolik2010@te.net.ua>
2015-05-21 21:05:50 +0300

    calc

---
+++ first/src/com/hillel/Java/oopBasics/Calc.java
@@ -0,0 +1,28 @@
+package com.hillel.Java.oopBasics;
+
+import javax.swing.*;
+
+/**
+ * Created by abozhko on 21.05.2015.
+ */
+public class Calc {
+    private JButton a4Button;
+    private JPanel panel1;
+    private JButton a5Button;
+    private JButton a6Button;
+    private JButton a1Button;
+    private JButton a2Button;
+    private JButton a3Button;
+    private JButton a7Button;
+    private JButton a8Button;
+    private JButton a9Button;
+    private JTextField textField1;
+    private JButton button1;
+    private JButton button2;
+    private JButton a0Button;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton cButton;
+}
